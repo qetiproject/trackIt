e92bee37ae210ab951106c3279989fa9dc58ca32
qetiproject <ketideveloper@gmail.com>
2025-09-25 10:25:51 +0400

    loading todo

--- src/app/todos/todos.ts
+++ src/app/todos/todos.ts
@@ -1,25 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
+import { environment } from '../../environments/environment';
 import { Todo, TodoResultData } from '../models/todo.interface';
 
 @Component({
   selector: 'app-todos',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './todos.html',
   styleUrl: './todos.css'
 })
 export class Todos implements OnInit{
-
+  loading: boolean = false;
+  error: string = '';
   todos: Todo[] = [];
   http = inject(HttpClient);
 
   getTodos() {
-    this.http.get<TodoResultData>('https://dummyjson.com/todos')
-      .subscribe(result => {
-        this.todos = result.todos;
-        console.log(this.todos)
-      })
+    this.loading = true;
+    this.http.get<TodoResultData>(`${environment.baseUrl}/todos`)
+      .subscribe({
+        next: (result) => {
+          this.todos = result.todos;
+          this.loading = false;
+        },
+        error: (err) => {
+          this.error = err.message;
+          this.loading = false;
+        }
+    })
   }
 
   ngOnInit() {
